4cfb08475d52306a1a04f132dba1eb5387d9e0a2
Dakshlunia <72087624+Dakshlunia@users.noreply.github.com>
2021-01-11 14:56:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,6 +4,9 @@ var backgroundImg;
 var obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6, obstacle7, obstacle8, obstacle9, obstacle10;
 var obstaclesGroup;
 var score=0;
+var PLAY=1,END=0;
+var gameState=PLAY;
+
 
 function preload() {
     backgroundImg = loadImage("images/track.jpg");
@@ -21,18 +24,22 @@ function preload() {
 }
 
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(1200, 800);
 
     ground = createSprite(2000, 400, 4000, 800);
     ground.addImage(backgroundImg);
     player = createSprite(50, 200, 10, 40);
     player.addImage(playerImg);
+    player.rotation=90;
     console.log(player);
     obstaclesGroup= new Group();
+    player.debug=true;
 }
 
 function draw() {
     background(0);
+    if(gameState===PLAY){
+        score=score+1;
     if (keyDown(RIGHT_ARROW)) {
         ground.x = ground.x - 15
 
@@ -45,23 +52,48 @@ function draw() {
         player.y = player.y + 5
 
     }
+    if(ground.x<0){
+        ground.x=width/2;
+    }
+    if(player.isTouching(obstaclesGroup)){
+        gameState=END;
+        obstaclesGroup.setVelocityXEach(0);
+        obstaclesGroup.setLifetimeEach(-1);
+    }
     spawnObstacles();
+    spawnPolice();
+}
+drawSprites();
+textSize(30);
+fill('red');
+strokeWeight(3);
+ stroke(0);
+textAlign(CENTER);
+text('score: '+score,width-100,100)
     //camera.position.x=player.x;
     //camera.position.y=400;
-    drawSprites();
+    if(gameState===END){
+        textSize(60);
+        fill('red');
+        strokeWeight(3);
+        stroke(0);
+     textAlign(CENTER);
+     text('Game Over',width/2,height/2);
+    }
+    
 }
 
 
 function spawnObstacles() {
     if (frameCount % 60 === 0) {
-        var obstacle = createSprite(700, 400, 10, 40);
+        var obstacle = createSprite(width, 400, 10, 40);
         //obstacle.debug = true;
+        
         obstacle.velocityX = -(6 + 3 * score / 100);
         obstacle.y=Math.round(random(200,600));
-        obstacle.rotation = 90;
-        obstacle.rotation = 90;
+        obstacle.rotation = -90;
         //generate random obstacles
-        var rand = Math.round(random(1, 10));
+        var rand = Math.round(random(1, 8));
         switch (rand) {
             case 1: obstacle.addImage(obstacle1);
                 break;
@@ -71,25 +103,48 @@ function spawnObstacles() {
                 break;
             case 4: obstacle.addImage(obstacle4);
                 break;
-            case 5: obstacle.addImage(obstacle5);
+            case 5: obstacle.addImage(obstacle7);
                 break;
-            case 6: obstacle.addImage(obstacle6);
+            case 6: obstacle.addImage(obstacle8);
                 break;
-            case 7: obstacle.addImage(obstacle7);
+            case 7: obstacle.addImage(obstacle9);
                 break;
-            case 8: obstacle.addImage(obstacle8);
-                break;
-            case 9: obstacle.addImage(obstacle9);
-                break;
-            case 10: obstacle.addImage(obstacle10);
+            case 8: obstacle.addImage(obstacle10);
                 break;
             default: break;
         }
-
+       // obstacle.scale=2.5;
         //assign scale and lifetime to the obstacle           
-        obstacle.scale = 0.5;
+        obstacle.debug=true;
+        obstacle.setCollider('rectangle',0,20,50,120);
         obstacle.lifetime = 300;
+        
         //add each obstacle to the group
         obstaclesGroup.add(obstacle);
+
+    }
+}
+function spawnPolice() {
+    if (frameCount % 120 === 0) {
+        var police = createSprite(width, 400, 10, 40);
+        //obstacle.debug = true;
+        
+       police.velocityX = -(8 + 3 * score / 100);
+       police.y=Math.round(random(200,600));
+       police.rotation = -90;
+        //generate random obstacles
+        var rand = Math.round(random(5, 6));
+        switch (rand) {
+            case 5:police.addImage(obstacle5);
+                break;
+            case 6:police.addImage(obstacle6);
+                break;
+            default: break;
+        }
+       // obstacle.scale=2.5;
+        //assign scale and lifetime to the obstacle           
+        police.lifetime = 300;
+        //add each obstacle to the group
+        obstaclesGroup.add(police);
     }
 }
